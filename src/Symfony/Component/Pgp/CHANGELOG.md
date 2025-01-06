5d5082feb91f439fc6224016929311494d0381e3
PuLLi <112799107+the-pulli@users.noreply.github.com>
2025-01-06 14:54:52 +0100

    First draft of symfony/pgp

---
+++ src/Symfony/Component/Pgp/CHANGELOG.md
@@ -0,0 +1,7 @@
+CHANGELOG
+=========
+
+7.3
+-----
+
+ * Introduced the component as experimental
---
+++ src/Symfony/Component/Pgp/PgpEncrypter.php
@@ -0,0 +1,266 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Pgp;
+
+use Symfony\Component\Mime\Header\Headers;
+use Symfony\Component\Mime\Header\MailboxListHeader;
+use Symfony\Component\Mime\Message;
+use Symfony\Component\Mime\Part\Multipart\MixedPart;
+use Symfony\Component\Pgp\Exception\PgpBadPassphraseException;
+use Symfony\Component\Pgp\Exception\PgpFileException;
+use Symfony\Component\Pgp\Exception\PgpGeneralException;
+use Symfony\Component\Pgp\Exception\PgpKeyNotFoundException;
+use Symfony\Component\Pgp\Mime\Part\Multipart\PgpEncryptedPart;
+use Symfony\Component\Pgp\Mime\Part\Multipart\PgpSignedPart;
+use Symfony\Component\Pgp\Mime\Part\PgpEncryptedInitializationPart;
+use Symfony\Component\Pgp\Mime\Part\PgpEncryptedMessagePart;
+use Symfony\Component\Pgp\Mime\Part\PgpKeyPart;
+use Symfony\Component\Pgp\Mime\Part\PgpSignaturePart;
+use Symfony\Component\Pgp\Mime\Traits\PgpSigningTrait;
+
+/*
+ * @author PuLLi <the@pulli.dev>
+ */
+final class PgpEncrypter
+{
+    use PgpSigningTrait;
+
+    private \Crypt_GPG $gpg;
+
+    private Headers $headers;
+
+    private ?string $signingKey = null;
+
+    public string $signature = '';
+
+    public string $signed = '';
+
+    /**
+     * @throws PgpFileException
+     * @throws PgpGeneralException
+     */
+    public function __construct(array $options = [])
+    {
+        try {
+            $this->gpg = new \Crypt_GPG(
+                array_merge(
+                    $options,
+                    [
+                        'cipher-algo' => 'AES256',
+                        'digest-algo' => 'SHA512',
+                    ],
+                )
+            );
+        } catch (\Crypt_GPG_FileException $e) {
+            throw new PgpFileException($e->getMessage());
+        } catch (\PEAR_Exception $e) {
+            throw new PgpGeneralException($e->getMessage());
+        }
+    }
+
+    public function signingKey(string $keyIdentifier): void
+    {
+        $this->signingKey = $keyIdentifier;
+    }
+
+    /**
+     * @throws PgpBadPassphraseException
+     * @throws PgpKeyNotFoundException
+     * @throws PgpGeneralException
+     */
+    public function encrypt(Message $message, bool $attachKey = false): Message
+    {
+        try {
+            return $this->encryptWithOrWithoutSigning($message, false, null, $attachKey);
+        } catch (\Crypt_GPG_BadPassphraseException $e) {
+            throw new PgpBadPassphraseException($e->getMessage());
+        } catch (\Crypt_GPG_KeyNotFoundException $e) {
+            throw new PgpKeyNotFoundException($e->getMessage());
+        } catch (\Crypt_GPG_Exception $e) {
+            throw new PgpGeneralException($e->getMessage());
+        }
+    }
+
+    /**
+     * @throws PgpBadPassphraseException
+     * @throws PgpKeyNotFoundException
+     * @throws PgpGeneralException
+     */
+    public function encryptAndSign(Message $message, ?string $passphrase = null, bool $attachKey = false): Message
+    {
+        try {
+            return $this->encryptWithOrWithoutSigning($message, true, $passphrase, $attachKey);
+        } catch (\Crypt_GPG_BadPassphraseException $e) {
+            throw new PgpBadPassphraseException($e->getMessage());
+        } catch (\Crypt_GPG_KeyNotFoundException $e) {
+            throw new PgpKeyNotFoundException($e->getMessage());
+        } catch (\Crypt_GPG_Exception $e) {
+            throw new PgpGeneralException($e->getMessage());
+        }
+    }
+
+    /**
+     * @throws PgpBadPassphraseException
+     * @throws PgpKeyNotFoundException
+     * @throws PgpGeneralException
+     */
+    private function encryptWithOrWithoutSigning(Message $message, bool $sign = false, ?string $passphrase = null, bool $attachKey = false): Message
+    {
+        $this->headers = $message->getHeaders();
+        $body = $message->getBody();
+
+        foreach ($this->getRecipients() as $recipient) {
+            $this->gpg->addEncryptKey($recipient);
+        }
+
+        if ($attachKey) {
+            try {
+                $body = $this->attachPublicKey($message);
+            } catch (\Crypt_GPG_KeyNotFoundException $e) {
+                throw new PgpKeyNotFoundException($e->getMessage());
+            } catch (\Crypt_GPG_Exception $e) {
+                throw new PgpGeneralException($e->getMessage());
+            }
+        }
+
+        if ($sign) {
+            $this->gpg->addSignKey($this->determineSigningKey(), $passphrase);
+
+            try {
+                $body = $this->gpg->encryptAndSign($body->toString());
+            } catch (\Crypt_GPG_BadPassphraseException $e) {
+                throw new PgpBadPassphraseException($e->getMessage());
+            } catch (\Crypt_GPG_KeyNotFoundException $e) {
+                throw new PgpKeyNotFoundException($e->getMessage());
+            } catch (\Crypt_GPG_Exception $e) {
+                throw new PgpGeneralException($e->getMessage());
+            }
+        } else {
+            try {
+                $body = $this->gpg->encrypt($body->toString());
+            } catch (\Crypt_GPG_KeyNotFoundException $e) {
+                throw new PgpKeyNotFoundException($e->getMessage());
+            } catch (\Crypt_GPG_Exception $e) {
+                throw new PgpGeneralException($e->getMessage());
+            }
+        }
+
+        $part = new PgpEncryptedPart(
+            new PgpEncryptedInitializationPart(),
+            new PgpEncryptedMessagePart($body)
+        );
+
+        return new Message($this->headers, $part);
+    }
+
+    /**
+     * @throws PgpBadPassphraseException
+     * @throws PgpKeyNotFoundException
+     * @throws PgpGeneralException
+     */
+    public function sign(Message $message, ?string $passphrase = null, bool $attachKey = false): Message
+    {
+        $this->headers = $message->getHeaders();
+        $body = $message->getBody()->toString();
+        $messagePart = $message->getBody();
+
+        if ($attachKey) {
+            try {
+                $mixed = $this->attachPublicKey($message);
+            } catch (\Crypt_GPG_KeyNotFoundException $e) {
+                throw new PgpKeyNotFoundException($e->getMessage());
+            } catch (\Crypt_GPG_Exception $e) {
+                throw new PgpGeneralException($e->getMessage());
+            }
+
+            $body = $mixed->toString();
+            $messagePart = $mixed;
+        }
+
+        // TODO: find a way to normalize Message body and pass it along to PGPSignedPart
+        $body = $this->prepareMessageForSigning($messagePart, $body);
+        $this->signed = $body;
+
+        $this->gpg->addSignKey($this->determineSigningKey(), $passphrase);
+
+        try {
+            $signature = $this->gpg->sign($body, \Crypt_GPG::SIGN_MODE_DETACHED);
+        } catch (\Crypt_GPG_BadPassphraseException $e) {
+            throw new PgpBadPassphraseException($e->getMessage());
+        } catch (\Crypt_GPG_KeyNotFoundException $e) {
+            throw new PgpKeyNotFoundException($e->getMessage());
+        } catch (\Crypt_GPG_Exception $e) {
+            throw new PgpGeneralException($e->getMessage());
+        }
+
+        $this->signature = $signature;
+        $part = new PgpSignedPart(
+            $messagePart,
+            new PgpSignaturePart($signature)
+        );
+
+        return new Message($this->headers, $part);
+    }
+
+    /**
+     * @throws PgpKeyNotFoundException
+     * @throws PgpGeneralException
+     */
+    private function attachPublicKey(Message $message): MixedPart
+    {
+        try {
+            $publicKey = $this->gpg->exportPublicKey($this->determineSigningKey());
+        } catch (\Crypt_GPG_KeyNotFoundException $e) {
+            throw new PgpKeyNotFoundException($e->getMessage());
+        } catch (\Crypt_GPG_Exception $e) {
+            throw new PgpGeneralException($e->getMessage());
+        }
+        $key = new PgpKeyPart($publicKey);
+
+        // TODO: find more elegant way than to create another MixedPart, if Message is already a MixedPart
+        return new MixedPart($message->getBody(), $key);
+    }
+
+    private function getRecipients(): array
+    {
+        $recipients = [
+            $this->getAddresses('to'),
+            $this->getAddresses('cc'),
+            $this->getAddresses('bcc'),
+        ];
+
+        return array_merge(...$recipients);
+    }
+
+    private function getFrom(): string
+    {
+        return $this->getAddresses('from')[0];
+    }
+
+    private function getAddresses(string $type): array
+    {
+        $addresses = [];
+        $addressType = $this->headers->get($type);
+        if ($addressType instanceof MailboxListHeader) {
+            foreach ($addressType->getAddresses() as $address) {
+                $addresses[] = $address->getAddress();
+            }
+        }
+
+        return $addresses;
+    }
+
+    private function determineSigningKey(): string
+    {
+        return $this->signingKey ?? $this->getFrom();
+    }
+}
